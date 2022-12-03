96ec1d237508f1bbba3e9f07a442866ea0c6c4c5
USER <user@ElPatron.local>
2022-12-03 12:22:30 +0300

    added more end points

--- transactions/urls.py
+++ transactions/urls.py
@@ -5,5 +5,8 @@ app_name = 'transactions'
 urlpatterns = [
     path('all_transactions/', get_all_transactions),
     path('create_transactions/', create_transaction),
+    path('update_transaction/<str:uid>/', update_transaction),
+    path('get_transaction/<str:uid>/', get_transaction),
+    path('delete_transaction/<str:uid>/', delete_transaction),
     
 ]
