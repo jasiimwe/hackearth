cdf44c32c4c8dcda95dfed06cb407424ff9d42e2
isaacn123 <nsambai72@gmail.com>
2022-12-03 17:34:05 +0300

    add to serializer

--- transactions/serializer.py
+++ transactions/serializer.py
@@ -4,7 +4,7 @@ from .models import Transactions
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transactions
-        fields = ('event_id', 'transaction_uid', 'transaction_amount','original_amount','is_partial','currency_code','status',
+        fields = ('event_id', 'transaction_uid','payment_id', 'organisation_id', 'transaction_amount','original_amount','is_partial','currency_code','status',
         'description','merchant_reference_id','customer_token','payment_method')
 
         
