8df30d8cf19f95f72d132f8f2963aeb380a28159
isaacn123 <nsambai72@gmail.com>
2022-12-05 12:10:16 +0300

    adding user id

--- transactions/models.py
+++ transactions/models.py
@@ -6,6 +6,7 @@ import uuid
 class Transactions(TimestampedModel):
     event_id = models.CharField(max_length=225)
     transaction_uid = models.UUIDField(default=uuid.uuid4, editable=False)
+    user_id = models.IntegerField(blank=True, null=True)
     payment_id = models.CharField(max_length=225, null=True)
     organisation_id = models.IntegerField(max_length=225, null=True)
     transaction_amount = models.IntegerField(blank=True, null=True)
