83b9b43271a16d594e60b6e259dcb55059087e72
isaacn123 <nsambai72@gmail.com>
2022-12-07 01:05:05 +0300

    fixing tranctiong comments

--- transactions/urls.py
+++ transactions/urls.py
@@ -5,12 +5,12 @@ app_name = 'transactions'
 urlpatterns = [
     path('all_transactions/', get_all_transactions),
     path('create_transactions/', create_transaction),
-<<<<<<< HEAD
+# <<<<<<< HEAD
     path('get_transaction_event/<str:event_uid>/', get_all_transactions_event),
-=======
+# =======
     path('update_transaction/<str:uid>/', update_transaction),
     path('get_transaction/<str:uid>/', get_transaction),
     path('delete_transaction/<str:uid>/', delete_transaction),
->>>>>>> d15af503137f15f080b313a19af07597e80eebfd
+# >>>>>>> d15af503137f15f080b313a19af07597e80eebfd
     
 ]
